1d82686d7bb430270b3711b1fd53c96bc6aa2725
4TASKW67 <4taskw67@solent.ac.uk>
2021-12-28 18:45:13 +0000

    Added a test for two basket implementations, one as a list and one as a hashmap

---
+++ ae2/model/src/test/java/org/solent/com504/oodd/cart/model/dto/ShoppingItemTest/ShoppingItemTest.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.solent.com504.oodd.cart.model.dto.ShoppingItemTest;
+
+import java.util.UUID;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.solent.com504.oodd.cart.model.dto.ShoppingItem;
+
+/**
+ *
+ * @author Will
+ */
+public class ShoppingItemTest {
+    final static Logger LOG = LogManager.getLogger(ShoppingItemTest.class);
+
+    @Test
+    public void testShoppingItemLookup() {
+        // Finding a shopping item in an ArrayList data structure
+        ArrayList<ShoppingItem> basketList = new ArrayList<ShoppingItem>();
+        
+        ShoppingItem shoppingItem1 = new ShoppingItem();
+        shoppingItem1.setName("item 1");
+        shoppingItem1.setPrice(100.1);
+        shoppingItem1.setQuantity(1);
+        shoppingItem1.setUuid(UUID.randomUUID().toString());
+        
+        ShoppingItem shoppingItem2 = new ShoppingItem();
+        shoppingItem2.setName("item 2");
+        shoppingItem2.setPrice(50.0);
+        shoppingItem2.setQuantity(4);
+        shoppingItem2.setUuid(UUID.randomUUID().toString());
+        
+        ShoppingItem shoppingItem3 = new ShoppingItem();
+        shoppingItem3.setName("item 3");
+        shoppingItem3.setPrice(50.0);
+        shoppingItem3.setQuantity(4);
+        shoppingItem3.setUuid(UUID.randomUUID().toString());
+        
+        basketList.add(shoppingItem1);
+        basketList.add(shoppingItem2);
+        basketList.add(shoppingItem3);
+        
+        String uuidToFind = basketList.get(2).getUuid();
+        LOG.debug("attemping to find " + uuidToFind + " in the list basket.");
+        
+        boolean found = false;
+        for (ShoppingItem item : basketList){
+            if (uuidToFind.equals(item.getUuid())) {
+                LOG.debug("found the item.");
+                found = true;
+                break;
+            }
+        }
+        assertTrue(found);
+        
+        // Finding a shopping item in a HashMap data structure
+        LinkedHashMap<String, ShoppingItem> basketTable = new LinkedHashMap<String, ShoppingItem>();
+        
+        basketTable.put(shoppingItem1.getUuid(), shoppingItem1);
+        basketTable.put(shoppingItem2.getUuid(), shoppingItem2);
+        basketTable.put(shoppingItem3.getUuid(), shoppingItem3);
+        
+        LOG.debug("attemping to find " + uuidToFind + " in the dict basket.");
+        ShoppingItem foundItem = basketTable.get(uuidToFind);
+        
+        found = false;
+        if (foundItem != null) {
+            found = true;
+        }
+        
+        assertTrue(found);
+    }
+}
